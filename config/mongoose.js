5a8a1d887787ff3654267d4230acd3441f546071
YuZheLin1987 <billlinxx@gmail.com>
2023-02-12 23:14:22 +0800

    feat: add message partials

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 // connect to database
-console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
 const db = mongoose.connection
