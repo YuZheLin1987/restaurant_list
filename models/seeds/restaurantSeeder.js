61e59e404e95269e036601b81a40a68e73de57c2
YuZheLin1987 <billlinxx@gmail.com>
2023-02-13 23:42:56 +0800

    feat: add restaurantSeeder comment

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -19,8 +19,9 @@ const Restaurant = require('../restaurant')
 const User = require('../user')
 
 db.once('open', () => {
+  // 將每個在種子使用者中的user，建立帳號以及擁有的餐廳資料
   SEED_USERS.forEach(user => {
-    const userRestaurantId = user.restaurantId
+    const userRestaurantId = user.restaurantId  // 抓出使用者擁有的餐廳
     bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(user.password, salt))
@@ -30,11 +31,12 @@ db.once('open', () => {
       }))
       .then(user => {
         const userId = user._id
+        // 用擁有的餐廳代號去過濾json檔資料，留下符合的
         const userRestaurant = restaurantData.filter(restaurant => {
           return userRestaurantId.includes(restaurant.id)
         })
         return Promise.all(Array.from(
-          { length: userRestaurant.length },
+          { length: userRestaurant.length },  // 依使用者擁有的餐廳量而定，依序建立餐廳清單
           (_, i) => {
             Restaurant.create({
               name: userRestaurant[i].name,
